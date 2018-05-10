f3b1f6e82fb2e6582868a4090fa110b62358316b
Andres AG <aa1399@my.bristol.ac.uk>
2018-05-10 16:35:53 +0100

    Remove use of printf from re module

--- extmod/re1.5/dumpcode.c
+++ extmod/re1.5/dumpcode.c
@@ -4,62 +4,80 @@
 
 #include "re1.5.h"
 
+#include "py/mpconfig.h"
+
+#if MICROPY_DEBUG_VERBOSE // print debugging info
+#define DEBUG_printf printf
+#else // don't print debugging info
+#define DEBUG_printf(...) (void)0
+#endif
+
 void re1_5_dumpcode(ByteProg *prog)
 {
     int pc = 0;
     char *code = prog->insts;
     while (pc < prog->bytelen) {
-                printf("%2d: ", pc);
+                DEBUG_printf("%2d: ", pc);
                 switch(code[pc++]) {
                 default:
                         assert(0);
 //                        re1_5_fatal("printprog");
                 case Split:
-                        printf("split %d (%d)\n", pc + (signed char)code[pc] + 1, (signed char)code[pc]);
+                        DEBUG_printf("split %d (%d)\n",
+                            pc + (signed char)code[pc] + 1,
+                            (signed char)code[pc]);
                         pc++;
                         break;
                 case RSplit:
-                        printf("rsplit %d (%d)\n", pc + (signed char)code[pc] + 1, (signed char)code[pc]);
+                        DEBUG_printf("rsplit %d (%d)\n",
+                            pc + (signed char)code[pc] + 1,
+                            (signed char)code[pc]);
                         pc++;
                         break;
                 case Jmp:
-                        printf("jmp %d (%d)\n", pc + (signed char)code[pc] + 1, (signed char)code[pc]);
+                        DEBUG_printf("jmp %d (%d)\n",
+                            pc + (signed char)code[pc] + 1,
+                            (signed char)code[pc]);
                         pc++;
                         break;
                 case Char:
-                        printf("char %c\n", code[pc++]);
+                        DEBUG_printf("char %c\n", code[pc]);
+                        pc++;
                         break;
                 case Any:
-                        printf("any\n");
+                        DEBUG_printf("any\n");
                         break;
                 case Class:
                 case ClassNot: {
                         int num = code[pc];
-                        printf("class%s %d", (code[pc - 1] == ClassNot ? "not" : ""), num);
+                        DEBUG_printf("class%s %d",
+                            (code[pc - 1] == ClassNot ? "not" : ""), num);
                         pc++;
                         while (num--) {
-                            printf(" 0x%02x-0x%02x", code[pc], code[pc + 1]);
+                            DEBUG_printf(" 0x%02x-0x%02x", code[pc], code[pc + 1]);
                             pc += 2;
                         }
-                        printf("\n");
+                        DEBUG_printf("\n");
                         break;
                 }
                 case NamedClass:
-                        printf("namedclass %c\n", code[pc++]);
+                        DEBUG_printf("namedclass %c\n", code[pc]);
+                        pc++;
                         break;
                 case Match:
-                        printf("match\n");
+                        DEBUG_printf("match\n");
                         break;
                 case Save:
-                        printf("save %d\n", (unsigned char)code[pc++]);
+                        DEBUG_printf("save %d\n", (unsigned char)code[pc]);
+                        pc++;
                         break;
                 case Bol:
-                        printf("assert bol\n");
+                        DEBUG_printf("assert bol\n");
                         break;
                 case Eol:
-                        printf("assert eol\n");
+                        DEBUG_printf("assert eol\n");
                         break;
                 }
     }
-    printf("Bytes: %d, insts: %d\n", prog->bytelen, prog->len);
+    DEBUG_printf("Bytes: %d, insts: %d\n", prog->bytelen, prog->len);
 }
