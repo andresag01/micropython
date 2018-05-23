1fe22e48a35df503ef878388d93f4e25c7810ee9
Andres AG <aa1399@my.bristol.ac.uk>
2018-05-23 18:03:05 +0100

    Sure port: reconcile gc and no gc memmove and memcpy

--- ports/sure/utils.c
+++ ports/sure/utils.c
@@ -21,31 +21,165 @@ void *_sbrk_r(size_t sizeBytes)
     return prev_heap;
 }
 
-void *memcpy(void * restrict dst0, const void * restrict src0, size_t len0)
+static void unaligned_memcpy(void * restrict dst0,
+                             const void * restrict src0,
+                             size_t len0)
 {
-  char *dst = (char *) dst0;
-  char *src = (char *) src0;
+    char *dst = (char *) dst0;
+    char *src = (char *) src0;
 
-  void *save = dst0;
-
-  while (len0--)
+    while (len0--)
     {
-      *dst++ = *src++;
+        *dst++ = *src++;
     }
-
-  return save;
 }
 
-char *strchr(const char *s1, int i)
+static void aligned_memcpy(void * restrict dst0,
+                           const void * restrict src0,
+                           size_t len0)
 {
-  const unsigned char *s = (const unsigned char *)s1;
-  unsigned char c = i;
+    /* Move the first unaligned portion in byte chunks */
+    size_t byteOffset = ((intptr_t)dst0) & 3;
+    char *dst = (char *)dst0;
+    char *src = (char *)src0;
 
-  while (*s && *s != c)
-    s++;
-  if (*s == c)
-    return (char *)s;
-  return NULL;
+    while (byteOffset--)
+    {
+        *dst++ = *src++;
+        len0--;
+    }
+
+    /* Move the aligned blocks in word chunks */
+    uint32_t *dstWord = (uint32_t *)dst;
+    uint32_t *srcWord = (uint32_t *)src;
+
+    while (len0 >= sizeof(uint32_t))
+    {
+        *dstWord++ = *srcWord++;
+        len0 -= sizeof(uint32_t);
+    }
+
+    /* Move the last unaligned portion in byte chunks */
+    dst = (char *)dstWord;
+    src = (char *)srcWord;
+
+    while (len0--)
+    {
+        *dst++ = *src++;
+    }
+}
+
+void *memcpy(void * restrict dst0, const void * restrict src0, size_t len0)
+{
+    void *save = dst0;
+
+    size_t dstByteOffset = ((uintptr_t)dst0) & 3;
+    size_t srcByteOffset = ((uintptr_t)src0) & 3;
+
+    if (dstByteOffset == srcByteOffset)
+    {
+        /* Source and destination alignment is the same */
+        aligned_memcpy(dst0, src0, len0);
+    }
+    else
+    {
+        /*
+         * Source and destination alignment is NOT the same. Pointer flags will
+         * be cleared in destination
+         */
+        unaligned_memcpy(dst0, src0, len0);
+    }
+
+    return save;
+}
+
+static void unaligned_back_memcpy(void *dst_void,
+                                  const void *src_void,
+                                  size_t length)
+{
+    char *dst = (char *)dst_void;
+    const char *src = (const char *)src_void;
+
+    src += length;
+    dst += length;
+
+    while (length--)
+    {
+        *--dst = *--src;
+    }
+}
+
+static void aligned_back_memcpy(void *dst_void,
+                                const void *src_void,
+                                size_t length)
+{
+    char *dst = (char *)dst_void;
+    const char *src = (const char *)src_void;
+
+    dst += length;
+    src += length;
+
+    /* Move the back unaligned portion in byte chunks */
+    size_t byteOffset = ((uintptr_t)dst) & 3;
+
+    while (byteOffset--)
+    {
+        *--dst = *--src;
+        length--;
+    }
+
+    /* Move the aligned blocks in word chunks */
+    uint32_t *dstWord = (uint32_t *)dst;
+    const uint32_t *srcWord = (const uint32_t *)src;
+
+    while (length >= sizeof(uint32_t))
+    {
+        *--dstWord = *--srcWord;
+        length -= sizeof(uint32_t);
+    }
+
+    /* Move the front unaligned portion in byte chunks */
+    dst = (char *)dstWord;
+    src = (const char *)srcWord;
+
+    while (length--)
+    {
+        *--dst = *--src;
+    }
+}
+
+void *memmove(void *dst_void, const void *src_void, size_t length)
+{
+    char *dst = dst_void;
+    const char *src = src_void;
+
+    size_t dstByteOffset = ((uintptr_t)dst_void) & 3;
+    size_t srcByteOffset = ((uintptr_t)src_void) & 3;
+
+    if (src < dst && dst < src + length)
+    {
+        /* Back to front */
+        if (dstByteOffset == srcByteOffset)
+        {
+            /* Source and destination alignment is the same */
+            aligned_back_memcpy(dst_void, src_void, length);
+        }
+        else
+        {
+            /*
+             * Source and destination alignment is NOT the same. Pointer flags
+             * will be cleared in destination
+             */
+            unaligned_back_memcpy(dst_void, src_void, length);
+        }
+    }
+    else
+    {
+        /* Front to back */
+        memcpy(dst_void, src_void, length);
+    }
+
+    return dst_void;
 }
 
 int memcmp(const void * m1, const void *m2, size_t n)
@@ -65,30 +199,16 @@ int memcmp(const void * m1, const void *m2, size_t n)
   return 0;
 }
 
-void *memmove(void *dst_void, const void *src_void, size_t length)
+char *strchr(const char *s1, int i)
 {
-  char *dst = dst_void;
-  const char *src = src_void;
+  const unsigned char *s = (const unsigned char *)s1;
+  unsigned char c = i;
 
-  if (src < dst && dst < src + length)
-    {
-      /* Have to copy backwards */
-      src += length;
-      dst += length;
-      while (length--)
-	{
-	  *--dst = *--src;
-	}
-    }
-  else
-    {
-      while (length--)
-	{
-	  *dst++ = *src++;
-	}
-    }
-
-  return dst_void;
+  while (*s && *s != c)
+    s++;
+  if (*s == c)
+    return (char *)s;
+  return NULL;
 }
 
 size_t strlen(const char *str)
